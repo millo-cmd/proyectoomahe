a0b00ab853e876d64987f4413f82ad0f5b72c982
millo-cmd <3612emiliogonzalez@gmail.com>
2024-11-25 00:08:57 -0600

    navegacion verificada con rol

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
+import { jwtDecode } from 'jwt-decode'
+
+import '../styles/navbar.css'
+ 
 
 const NavBar = () => {
+    const [userRol, setUserRol] = useState();
+
+
+    useEffect(() => {
+        const token = localStorage.getItem('authToken');
+
+        if(token){
+            try{
+                const data = jwtDecode(token);
+                const rol = data.rol;
+                setUserRol(rol);
+
+                console.log(rol);
+                
+                
+            }catch(e){
+                console.log('error al decodificar el token: ', e.message);
+            }
+        }else{
+            console.log('No se encontró un token en el almacenamiento local');
+        }
+    })
+
+
     return (
-        <div>
-            <h1>aaaaaaa</h1>
+        <div className="divBar">
+            <ul>
+                {
+                    userRol === 'Administrador Org' && (
+                        <li> <a href=""> aña</a></li>
+                    )
+                }
+            </ul>
+            
             
         </div>
     );
