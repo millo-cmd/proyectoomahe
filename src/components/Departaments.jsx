5c44c6e32397ac82edf78daa835fc7bf2d6a9ac5
millo-cmd <3612emiliogonzalez@gmail.com>
2024-11-28 16:40:06 -0600

    post y put departament

--- src/components/Departaments.jsx
+++ src/components/Departaments.jsx
@@ -1,15 +1,26 @@
 import React, { useEffect, useState } from 'react'
 
 import axios from 'axios'
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 import NavBar from './Navbar';
+import ModalDep from './ModalDep'
 import '../styles/Departaments.css';
 
 const Departaments = () => {
 
     const [listDep, setListDep] = useState([]);
+    const [showForm, setShowForm] = useState(false);
+    const [departamentEdit, setDepartamentEdit] = useState(null);
+    const [ deleteDep, setDeleteDep ] = useState(false);
 
     const token = localStorage.getItem('authToken');
+
+    const handleEditDepartament = (departaments) => {
+        setDepartamentEdit(departaments);
+        setShowForm(true);
+    }
 
     const gettDepartament = () => {
         axios.get('http://localhost:4000/api/v1/department', {
@@ -26,14 +37,68 @@ const Departaments = () => {
     useEffect(() =>{
         gettDepartament();
     }, []);
+
+    const deleteDepartament = (depId) => {
+        const confirmDelete = window.confirm('¿Estás seguro de que deseas eliminar este deparamento?');
+        if(!confirmDelete){
+            return;
+        }
+
+        axios.delete(`http://localhost:4000/api/v1/department/delete/${depId}`,
+            {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            }).then((response) => {
+                if(response.status === 200){
+                    gettDepartament();
+                    setDeleteDep(true);
+                    toast.success('Departamento eliminado correctamente'); 
+                }
+            }).catch((err) => {
+                if (err.response) {
+                    switch (err.response.status) {
+                        case 404:
+                            toast.error('Departamento no encontrado');
+                            break;
+                        case 403:
+                            toast.error('No tienes permisos para eliminar este Departamento');
+                            break;
+                        default:
+                            toast.error('Error al eliminar el Departamento');
+                    }
+                } else if (err.request) {
+                    toast.error('No se pudo conectar con el servidor');
+                } else {
+                    // Algo salió mal al configurar la solicitud
+                    toast.error('Error inesperado al intentar eliminar');
+                }
+                
+                // Loguear el error para depuración
+                console.error('Error detallado:', err);
+            })
+
+    }
     
   return (
   <>
     <NavBar />
     <h2>Departamentos</h2>
-    <button>
+    <button onClick={() => {
+        setDepartamentEdit(null);
+        setShowForm(true);
+    }}>
         Agregar
     </button>
+
+    <ModalDep 
+        isOpen={showForm}
+        closeModal={() => setShowForm(false) }
+        tokenDep={token}
+        refreshDep={gettDepartament}
+        departamentEdit={departamentEdit}
+        deleteDep={deleteDep}
+    />
 
     <div className='contenedorDep'>
         {
@@ -41,6 +106,10 @@ const Departaments = () => {
                 <div key={departaments._id} className='card'>
                     <h3>{departaments.nombre}</h3>
                     <p>{departaments.codigo}</p>
+                    <div className='bbuttonDiv'>
+                        <button onClick={()=> deleteDepartament(departaments._id)}>Eliminar</button>
+                        <button onClick={()=> handleEditDepartament(departaments) }>Editar</button>
+                    </div>
                 </div>
             ))
         }
--- src/components/ModalDep.jsx
+++ src/components/ModalDep.jsx
@@ -1,43 +1,45 @@
-
 import React, { useEffect, useState } from 'react'
 
 import axios from 'axios';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+import { jwtDecode } from 'jwt-decode'
 
- const Modal = ({
+const ModalDep = ({ 
     isOpen, 
     closeModal, 
-    tokenOrg, 
-    refreshOrg,
-    organizationEdit
+    tokenDep, 
+    refreshDep, 
+    departamentEdit,
+    deleteDep
 }) => {
 
-    const [deleteOrg] = useState(false);
-    const [formData, setFormData] = useState({
+    
+    const[formData, setFormData] = useState({
         nombre: '',
-        tipo: '',
+        eliminado: deleteDep,
         codigo: ''
     });
 
-    useEffect(() =>{
-        if(organizationEdit){
+    useEffect(() => {
+        if(departamentEdit){
             setFormData({
-                nombre: organizationEdit.nombre || '',
-                tipo: organizationEdit.tipo || '',
-                codigo: organizationEdit.codigo || '',
-                liminado: deleteOrg
+                nombre: departamentEdit.nombre || '',
+                eliminado: deleteDep,
+                codigo: departamentEdit.codigo || ''
+                
             });
         }else {
             setFormData({
                 nombre: '',
-                tipo: '',
-                codigo: '',
-                eliminado: deleteOrg
+                eliminado: deleteDep,
+                codigo: ''
             })
         }
-    }, [organizationEdit])
+        console.log(jwtDecode(tokenDep));
+        
+    }, [departamentEdit])
 
     const handleChange = (e) => {
         const {name, value} = e.target;
@@ -50,59 +52,57 @@ import 'react-toastify/dist/ReactToastify.css';
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        if(organizationEdit){
-            axios.put(`http://localhost:4000/api/v1/organization/update/${organizationEdit._id}`, 
+        if(departamentEdit){
+            axios.put(`http://localhost:4000/api/v1/department/update/${tokenDep}`,
                 formData,
                 {
                     headers: {
-                        'Authorization': `Bearer ${tokenOrg}`,
+                        'Authorization': `Bearer ${tokenDep}`,
+                        'Content-Type': 'application/json'
+                    }
+                }
+            ).then((reponse) => {
+                toast.success('Departamento actualizado correctamente');
+                closeModal();
+                refreshDep();
+            }).catch((err) => {
+                handleError(err);
+            })
+        }else{
+            axios.post('http://localhost:4000/api/v1/department/add',
+                formData,
+                {
+                    headers: {
+                        'Authorization': `Bearer ${tokenDep}`,
                         'Content-Type': 'application/json'
                     }
                 }
             ).then((response) => {
-                toast.success('Organización actualizada correctamente');
+                toast.success('Departamento agregado correctamente');
                 closeModal();
-                refreshOrg();
-            }).catch((err) => {
+                refreshDep();
+            }).catch((err) =>{
                 handleError(err);
             });
-        } else{
-            axios.post('http://localhost:4000/api/v1/organization/add', 
-                formData,
-                {
-                    headers: {
-                        'Authorization': `Bearer ${tokenOrg}`,
-                        'Content-Type': 'application/json'
-                    }
-                }
-            ).then((response) => {
-                toast.success('Organización agregada correctamente');
-                closeModal();
-                refreshOrg();
-            }).catch((err) => {
-                handleError(err);
-            });
-        }
+        } 
     }
 
     const handleError = (err) => {
         if(err.response){
-            toast.error(err.response.data.message || 'Error al procesar la organización');
+            toast.error(err.response.data.message || 'Error al procesar el departamento');
         } else if (err.request){
             toast.error('No se pudo conectar con el servidor');
             console.log('Error al procesar la solicitud');   
         } else {
             toast.error('Error al procesar la solicitud');
         }
-    };
+    }
 
     if(!isOpen) return null;
-    
-
 
   return (
     <div>
-        <h3>{organizationEdit ? 'Editar Organización' : 'Agregar Organización'}</h3>
+        <h3>{departamentEdit ? 'Editar Departamento' : 'Agregar Departamento'}</h3>
         <form onSubmit={handleSubmit}>
                     <div>
                         <label htmlFor="name">Name</label>
@@ -111,17 +111,6 @@ import 'react-toastify/dist/ReactToastify.css';
                             id="name"
                             name="nombre"
                             value={formData.nombre}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
-                    <div>
-                        <label htmlFor="type">Type </label>
-                        <input
-                            type="text"
-                            id="type"
-                            name="tipo"
-                            value={formData.tipo}
                             onChange={handleChange}
                             required
                         />
@@ -138,13 +127,12 @@ import 'react-toastify/dist/ReactToastify.css';
                         />
                     </div>
                     <button type="submit">
-                        {organizationEdit ? 'Actualizar' : 'Agregar'}
+                        {departamentEdit ? 'Actualizar' : 'Agregar'}
                     </button>
-            </form>
-
+        </form>
         <button onClick={closeModal}>Cancelar</button>
     </div>
   )
 }
 
-export default Modal;
+export default ModalDep;
